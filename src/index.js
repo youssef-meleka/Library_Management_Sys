09d5a71a9a70119fd056610fc708dd54baa9fa86
youssef-meleka <youssefatef00@gmail.com>
2024-02-01 23:02:58 +0200

    rate limiter for borrowed books router

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ const bodyParser = require('body-parser');
 const bookRouter = require('./routes/bookRoutes');
 const borrowerRouter = require('./routes/borrowerRoutes');
 const borrowedBookRouter = require('./routes/borrowedBookRoutes');
+const rateLimit = require('express-rate-limit');
 
 dotenv.config();
 
 app.use(bodyParser.json());
 const PORT = process.env.PORT;
 
+//limits to 10 requests per 1 minute
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 10,
+});
+
+
 app.use(bookRouter);
 app.use(borrowerRouter);
-app.use(borrowedBookRouter);
+app.use(limiter,borrowedBookRouter); // apply limiter for those paths only
 
 app.listen(PORT, () => {
   console.log(`Welcome the Server is running on port ${PORT}`);
